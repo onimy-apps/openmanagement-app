5e4a7ee0912701cf43b1298afba0f05e7aa52a8c
Zafar Saleem <zafarsaleem3@gmail.com>
2019-02-15 15:02:04 -0500

    TASK: Added admin section

---
+++ src/components/admin/employees/employees.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Employees extends Component {
+  render() {
+    return (
+      <div>EMPLOYEES</div>
+    );
+  }
+}
+
+export default Employees;
