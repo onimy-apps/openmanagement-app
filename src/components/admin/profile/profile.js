d775e8f1c544d798bb892aba578a93805924f8b4
Zafar Saleem <zafarsaleem3@gmail.com>
2019-02-26 09:29:17 -0500

    FEATURE: Profile component with admin profile displayed

--- src/components/admin/profile/profile.js
+++ src/components/admin/profile/profile.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { adminProfileAction } from '../../../actions/admin';
+import { getCookie } from '../../../utils/cookies';
 
 class Profile extends Component {
+  state = {
+    cookies: {
+      id: getCookie('id'),
+      role: getCookie('role')
+    }
+  }
+
+  constructor(props) {
+    super(props);
+    this.props.dispatch(adminProfileAction(this.state.cookies));
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.adminProfile.hasOwnProperty('response')) {
+      return {
+        profile: nextProps.adminProfile.response
+      }
+    }
+
+    return {
+      profile: undefined
+    }
+  }
+
   render() {
+    if (this.state.profile === undefined) {
+      return <div>Loading...</div>
+    }
+
     return (
-      <div>PROFILE</div>
+      <div>
+        <ul>
+          <li>Name: {this.state.profile.name}</li>
+          <li>Email: {this.state.profile.email}</li>
+          <li>Username: {this.state.profile.username}</li>
+          <li>Role: {this.state.profile.role}</li>
+        </ul>
+      </div>
     );
   }
 }
 
-export default Profile;
+const mapStateToProps = (state) => (state);
+
+export default connect(mapStateToProps)(Profile);
