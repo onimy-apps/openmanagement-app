c50f61ccc891cdf637197246996fce8b65823631
Zafar Saleem <zafarsaleem3@gmail.com>
2019-03-06 06:35:05 -0500

    TASK: Replace Employees with Dashboard and welcome message

--- src/components/common/header/header.js
+++ src/components/common/header/header.js
@@ -5,16 +5,18 @@ import Employees from '../../admin/employees/employeesComponent';
 import New from '../../admin/new/newComponent';
 import Profile from '../../admin/profile/profileComponent';
 import Logout from '../../logoutComponent';
+import { getCookie } from '../../../utils/cookies';
 
 const Header = ({ match }) => (
   <div>
     <ul>
-      <li><Link to={`${match.path}/list`}>Employees</Link></li>
+      <li><Link to={`${match.path}/dashboard`}>Dashboard</Link></li>
       <li><Link to={`${match.path}/new`}>New</Link></li>
       <li><Link to={`${match.path}/profile`}>Profile</Link></li>
       <li><Link to={`${match.path}/logout`}>Logout</Link></li>
     </ul>
-    <Route path={`${match.path}/list`} component={Employees} />
+    <div>Welcome {getCookie('name')}</div>
+    <Route path={`${match.path}/dashboard`} component={Employees} />
     <Route path={`${match.path}/new`} component={New} />
     <Route path={`${match.path}/profile`} component={Profile} />
     <Route path={`${match.path}/logout`} component={Logout} />
