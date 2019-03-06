f2748769cb8d7086b43e9cbfdaa17f463905172d
Zafar Saleem <zafarsaleem@Zafars-MacBook-Pro.local>
2019-03-06 05:45:53 -0500

    FEATURE: Logout link

--- src/components/common/header/header.js
+++ src/components/common/header/header.js
@@ -4,6 +4,7 @@ import { Link, Route } from 'react-router-dom';
 import Employees from '../../admin/employees/employeesComponent';
 import New from '../../admin/new/newComponent';
 import Profile from '../../admin/profile/profileComponent';
+import Logout from '../../logoutComponent';
 
 const Header = ({ match }) => (
   <div>
@@ -11,10 +12,12 @@ const Header = ({ match }) => (
       <li><Link to={`${match.path}/list`}>Employees</Link></li>
       <li><Link to={`${match.path}/new`}>New</Link></li>
       <li><Link to={`${match.path}/profile`}>Profile</Link></li>
+      <li><Link to={`${match.path}/logout`}>Logout</Link></li>
     </ul>
     <Route path={`${match.path}/list`} component={Employees} />
     <Route path={`${match.path}/new`} component={New} />
     <Route path={`${match.path}/profile`} component={Profile} />
+    <Route path={`${match.path}/logout`} component={Logout} />
   </div>
 );
 
