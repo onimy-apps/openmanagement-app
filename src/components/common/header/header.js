5917977912a6cba756d90fd3a50818a8682a1689
Zafar Saleem <zafarsaleem3@gmail.com>
2019-03-09 10:53:21 -0500

    TASK: Different menu items for admin and managers

--- src/components/common/header/header.js
+++ src/components/common/header/header.js
@@ -7,12 +7,21 @@ import Profile from '../../admin/profile/profileComponent';
 import Logout from '../../logoutComponent';
 import { getCookie } from '../../../utils/cookies';
 import List from '../../employees/list/listComponent';
+import Project from '../../employees/project/createProjectComponent';
 
 const Header = ({ match }) => (
   <div>
     <ul>
       <li><Link to={`${match.path}/dashboard`}>Dashboard</Link></li>
-      <li><Link to={`${match.path}/new`}>New</Link></li>
+      <li>
+        {
+          (getCookie('role') === 'Admin')
+          ?
+          <Link to={`${match.path}/new`}>New</Link>
+          :
+          <Link to={`${match.path}/create`}>Create Project</Link>
+        }
+      </li>
       <li><Link to={`${match.path}/profile`}>Profile</Link></li>
       <li><Link to={`${match.path}/logout`}>Logout</Link></li>
     </ul>
@@ -21,6 +30,7 @@ const Header = ({ match }) => (
     <Route path={`${match.path}/new`} component={New} />
     <Route path={`${match.path}/profile`} component={Profile} />
     <Route path={`${match.path}/logout`} component={Logout} />
+    <Route path={`${match.path}/create`} component={Project} />
   </div>
 );
 
